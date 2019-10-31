0d6df5ad8791775c34cfe909d72b0b8288c60f1c
jakub-tomczak <jakub.tomczak@live.com>
2019-10-31 12:10:09 +0100

    Revert to original if swap is worsening the solution.

--- ATSP/src/Heuristics/GreedyHeuristic.cs
+++ ATSP/src/Heuristics/GreedyHeuristic.cs
@@ -4,14 +4,14 @@ using System;
 
 namespace ATSP.Heuristics{
     public class GreedyHeuristic: ATSPHeuristic{
-        
+
         public Permutators.DefaultSwapper Swapper = new Permutators.DefaultSwapper();
 
         public GreedyHeuristic() : base()
         {
 
         }
-        
+
         public override void Reset()
         {
             base.Reset();
@@ -22,27 +22,26 @@ namespace ATSP.Heuristics{
         public override void NextStep()
         {
             int size = Solution.Length;
-            var SourceSolution = Solution;
-            var BestSolution = Solution;
-            uint BestSolutionCost = CalculateCost();
-            uint CurrSolutionCost = BestSolutionCost;
+            currentCost = CalculateCost();
+            uint CurrSolutionCost = currentCost;
             for(int i = 0; i < size ; i++)
             {
                 for(int j = 0; j < size ; j++)
                 {
                     if(i != j){
-                        Solution = SourceSolution;
-                        Swapper.Swap(Solution,i,j);
+                        Swapper.Swap(Solution, i, j);
                         CurrSolutionCost = CalculateCost();
-                        if(BestSolutionCost > CurrSolutionCost){
-                            BestSolution = Solution;
-                            BestSolutionCost = CurrSolutionCost;
+                        if(CurrSolutionCost < currentCost)
+                        {
+                            currentCost = CurrSolutionCost;
+                        }
+                        else
+                        {
+                            Swapper.Swap(Solution, i, j);
                         }
                     }
                 }
             }
-            Solution = BestSolution;
-            PrintSolution();
             IsEnd = true;
         }
 
@@ -53,8 +52,8 @@ namespace ATSP.Heuristics{
                 cost += vertices[CurrSolution[i], CurrSolution[(i+1)%CurrSolution.Length]];
             }
             return cost;
-        } 
+        }
 
-
+        uint currentCost = 0;
     }
 }
