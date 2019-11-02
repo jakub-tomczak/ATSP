af088fae6bc12dff7dbd9b86301d1052d9deee3b
jakub-tomczak <jakub.tomczak@live.com>
2019-11-02 15:49:14 +0100

    Revert greedy.

---
+++ ATSP/src/Heuristics/GreedyHeuristic.cs
@@ -0,0 +1,51 @@
+using System;
+
+
+
+namespace ATSP.Heuristics{
+    public class GreedyHeuristic: ATSPHeuristic{
+
+        public Permutators.DefaultSwapper Swapper = new Permutators.DefaultSwapper();
+
+        public GreedyHeuristic() : base()
+        {
+
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+        }
+        public override bool IsEnd { get; protected set; }
+
+
+        public override void NextStep()
+        {
+            int size = Solution.Length;
+            currentCost = CalculateCost();
+            uint CurrSolutionCost = currentCost;
+            for(int i = 0; i < size ; i++)
+            {
+                for(int j = 0; j < size ; j++)
+                {
+                    if(i != j){
+                        Swapper.Swap(Solution, i, j);
+                        CurrSolutionCost = CalculateCost();
+                        if(CurrSolutionCost < currentCost)
+                        {
+                            currentCost = CurrSolutionCost;
+                            break;
+                        }
+                        else
+                        {
+                            Swapper.Swap(Solution, i, j);
+                        }
+                    }
+                }
+            }
+            IsEnd = true;
+        }
+
+        uint currentCost = 0;
+    }
+}
