cb5bde93e59336347ed9bd7814bf098ed830e02e
ledziu <mikolaj.sledz@gmail.com>
2019-11-06 18:39:11 +0100

    plot adjustment

--- Raport/draw_plots.py
+++ Raport/draw_plots.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 class PlotDrawer():
     def __init__(self, plots_path, display_plots):
         self.save_plots = True if plots_path is not None and plots_path != "" else False
         self.show_plots = display_plots
         self.save_directory = plots_path
 
-    def save_plot(self, name, extension='png'):
+    def save_plot(self, name, extension='png',instance=''):
         if not self.save_plots:
             return
         if name == "":
@@ -18,7 +19,8 @@ class PlotDrawer():
         if not os.path.exists(self.save_directory):
             if not os.makedirs(self.save_directory):
                 print("Failed to create directory for plots {}".format(self.save_directory))
-        plt.savefig(os.path.join(self.save_directory, '{}.{}'.format(name, extension)))
+        plt.title(name + ' ' + instance)
+        plt.savefig(os.path.join(self.save_directory, '{}_{}.{}'.format(name,instance, extension)))
 
     def show_plot(self):
         if self.show_plots:
@@ -30,6 +32,9 @@ class PlotDrawer():
         elif type == "max":
             return np.max
         return None
+    
+    def get_instance_name(self,data):
+        return data[0].instance.replace('\n','')
 
     def get_qualities(self, data):
         return np.array([[execution.quality for execution in x.executions] for x in data])
@@ -49,14 +54,14 @@ class PlotDrawer():
         best_qualities = np.max(self.get_qualities(data), axis=1)
         names = self.get_alg_names(data)
         sns.boxplot(data=mean_qualities.tolist()).set_xticklabels(names)
+        instance_name = self.get_instance_name(data)
         # self.show_plot()
-        self.save_plot("mean_quality")
+        self.save_plot("mean_quality", instance=instance_name)
 
-        print(mean_qualities)
-
-        sns.boxplot(data=best_qualities.tolist())
+        plt.figure(0)
+        plt.plot(names,best_qualities,'o',color='red')
         # self.show_plot()
-        self.save_plot("best_qualities")
+        self.save_plot("best_qualities", instance=instance_name)
 
     def draw_time_plots(self, data):
         print("drawing time plots")
