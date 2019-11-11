c3bcc2133a8d933494d9c2425d1faa8629121035
jakub-tomczak <jakub.tomczak@live.com>
2019-11-11 21:51:30 +0100

    Avoid drawing plots with no data.

--- Raport/draw_plots.py
+++ Raport/draw_plots.py
@@ -85,11 +85,12 @@ class PlotDrawer():
         for experiment_result in data:
             best_value_experiment = min(experiment_result.executions, key=lambda x: x.cost)
             plt.plot(best_value_experiment.intermediate_costs, label=experiment_result.name)
-        axes = plt.gca()
-        axes.axhline(y=data[0].executions[0].best_known_cost, linestyle='--', linewidth=.5, color='magenta', label='best known cost')
-        plt.legend()
-        self.save_plot("intermediate_costs".format(experiment_result.name), instance=experiment_result.instance)
-        self.show_plot()
+        if len(data) > 0 and len(data.executions) > 0:
+            axes = plt.gca()
+            axes.axhline(y=data[0].executions[0].best_known_cost, linestyle='--', linewidth=.5, color='magenta', label='best known cost')
+            plt.legend()
+            self.save_plot("intermediate_costs".format(experiment_result.name), instance=experiment_result.instance)
+            self.show_plot()
 
     def draw_plots(self, data):
         print("drawing graphs")
