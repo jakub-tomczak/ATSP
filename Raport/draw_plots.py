1e67f57c6e50aa14c74d966415bb21cdf7f0ff50
ledziu <mikolaj.sledz@student.put.poznan.pl>
2019-11-11 23:32:15 +0100

    'nie umiem w wykresy'

--- Raport/draw_plots.py
+++ Raport/draw_plots.py
@@ -20,7 +20,7 @@ class PlotDrawer():
         if not os.path.exists(self.save_directory):
             if not os.makedirs(self.save_directory):
                 print("Failed to create directory for plots {}".format(self.save_directory))
-        plt.title(name + ' ' + instance)
+        #plt.title(name + ' ' + instance)
         plt.savefig(os.path.join(self.save_directory, '{}_{}.{}'.format(name,instance, extension)))
 
     def show_plot(self):
@@ -59,27 +59,27 @@ class PlotDrawer():
         if len(data) < 1:
             return
         print("drawing quality plots")
+        plt.clf()
         mean_qualities = self.get_qualities(data)
         best_qualities = np.max(self.get_qualities(data), axis=1)
         names = self.get_alg_names(data)
         sns.boxplot(data=mean_qualities.tolist()).set_xticklabels(names)
         instance_name = self.get_instance_name(data)
+        plt.xlabel('Algorytmy')
+        plt.ylabel('Średnia jakość rozwiązania')
         # self.show_plot()
         self.save_plot("mean_quality", instance=instance_name)
-
-        plt.figure(0)
-        plt.plot(names,best_qualities,'o',color='red')
+        plt.clf()
+        for i in range(len(names)):
+            plt.plot(names[i],best_qualities[i],'o')
+        plt.legend()
+        plt.xlabel('Algorytmy')
+        plt.ylabel('najlepsza jakość rozwiązania')
         # self.show_plot()
         self.save_plot("best_qualities", instance=instance_name)
 
-        print("drawing first last ")
-        plt.figure(2)
-        firsts, lasts = self.get_first_last_qualities(data)
-        for i in range(len(names)):
-            plt.plot(firsts[i],lasts[i],'o',label=names[i])
-        plt.legend()
+
         
-        self.save_plot("first_last",instance=instance_name)
 
     def draw_time_plots(self, data):
         for experiment_result in data:
@@ -90,12 +90,45 @@ class PlotDrawer():
             axes = plt.gca()
             axes.axhline(y=data[0].executions[0].best_known_cost, linestyle='--', linewidth=.5, color='magenta', label='best known cost')
             plt.legend()
-            self.save_plot("intermediate_costs".format(experiment_result.name), instance=experiment_result.instance)
+            plt.xlabel('Liczba iteracji')
+            plt.ylabel('Koszt rozwiązania')
+            self.save_plot("intermediate_costs".format(experiment_result.name), instance=self.get_instance_name(data))
             self.show_plot()
+
+    def draw_first_last_plots(self,data):
+        instance_name = self.get_instance_name(data)
+        firsts, lasts = self.get_first_last_qualities(data)
+        names = self.get_alg_names(data)
+        for i in range(len(names)):
+            plt.plot(firsts[i],lasts[i],'o',label=names[i])
+        plt.legend()
+        plt.xlabel('Jakość pierwotnego rozwiązania')
+        plt.ylabel('Jakość ostatecznego rozwiązania')
+        self.save_plot("first_last",instance=instance_name)
+
+    def draw_steps_quanted_results(self,data):
+        instance_name = self.get_instance_name(data)
+        firsts, lasts = self.get_first_last_qualities(data)
+        names = self.get_alg_names(data)
+        steps =  np.array([[execution.steps for execution in x.executions] for x in data])
+        calculated = np.array([[len(execution.intermediate_costs) for execution in x.executions] for x in data])
+        for i in range(len(names)):
+            plt.plot(steps[i],calculated[i],'o',label=names[i])
+        plt.xlabel('')
+        plt.ylabel('')
+        plt.legend()
+        self.save_plot("steps_results",instance=instance_name)
+                
 
     def draw_plots(self, data):
         if len(data) > 0:
             print("\n{}\ndrawing graphs for intance {}".format('*'*20, data[0].instance))
             self.draw_time_plots(data)
+            plt.clf()
             self.draw_quality_plots(data)
+            plt.clf()
+            self.draw_first_last_plots(data)
+            plt.clf()
+            self.draw_steps_quanted_results(data)
+
 
