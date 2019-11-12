7570b1a92f6be9ca6ed698968005149f154e8bd7
jakub-tomczak <jakub.tomczak@live.com>
2019-11-12 22:19:19 +0100

    Calculate permutation similarity.

--- Raport/utils.py
+++ Raport/utils.py
@@ -40,3 +40,33 @@ def load_instance_results(directory, files_extension):
         experiments_results.append(result)
     print("Read {} experiments".format(len(experiments_results)))
     return experiments_results
+
+
+def calculate_arrays_similarity(arr1, arr2):
+    return float(get_the_longest_subsequence(arr1, arr2)) / len(arr1)
+
+
+def find_starting_index(first_array, second_array):
+    for key, value in enumerate(second_array):
+        if first_array[0] == value:
+            return key
+    print("Matching value not found.")
+    return -1
+
+
+def get_the_longest_subsequence(arr1, arr2):
+    nearest_index = find_starting_index(arr1, arr2)
+    if nearest_index < 0:
+        exit(0)
+    arr1 = np.array(arr1)
+    arr2 = np.roll(np.array(arr2), -nearest_index)
+
+    result = np.zeros((len(arr1), len(arr2)))
+
+    for i in range(1, len(arr1)):
+        for j in range(1, len(arr2)):
+            if arr1[i] == arr2[j]:
+                result[i, j] = result[i-1, j-1]+1
+            else:
+                result[i, j] = max(result[i-1, j], result[i, j-1])
+    return 0 if result[-1, -1] <= 1 else result[-1, -1]+1
